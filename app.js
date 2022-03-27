c302764034530aafaa4caa4bef6ab1e432f6cf13
Hammer-01 <98681433+Hammer-01@users.noreply.github.com>
2022-03-27 20:08:07 +1000

    Update app.js

--- app.js
+++ app.js
@@ -16,12 +16,12 @@ var cnv;
 var setup = function() {
     cnv = createCanvas(windowWidth, windowHeight);
     console.log('Canvas id: ' + cnv.id());
+    cnv.elt.addEventListener('pointerdown', function(event) {
+        eraser = event.button === 5;
+    }, true);
+    
     strokeColour = color(255);
 };
-
-cnv.elt.addEventListener('pointerdown', function(event) {
-    eraser = event.button === 5;
-}, true);
 
 var mouseDragged = function() {
     if (eraser) {
