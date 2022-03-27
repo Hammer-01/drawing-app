e30ad513fa36d0e7800aae4adcbe697832a48fc9
Hammer-01 <98681433+Hammer-01@users.noreply.github.com>
2022-03-27 20:13:30 +1000

    Update app.js

--- app.js
+++ app.js
@@ -17,13 +17,18 @@ var setup = function() {
     cnv = createCanvas(windowWidth, windowHeight);
     console.log('Canvas id: ' + cnv.id());
     cnv.elt.addEventListener('pointerdown', function(event) {
+        console.log('pointerType: ' + event.pointerType);
+        console.log('Button: ' + event.button);
+        console.log('Eraser before: ' + eraser);
         eraser = event.button === 5;
+        console.log('Eraser after: ' + eraser);
     }, true);
     
     strokeColour = color(255);
 };
 
 var mouseDragged = function() {
+    console.log('Eraser in mouseDragged' + eraser);
     if (eraser) {
         noStroke();
         fill(255);
