3e0ce27936c1664f4dd207377958c63e8422b59b
Hammer-01 <98681433+Hammer-01@users.noreply.github.com>
2022-03-28 17:09:21 +1000

    Update app.js

--- app.js
+++ app.js
@@ -27,13 +27,14 @@ var draw = function() {
     background(255);
     stroke(strokeColour); // move inside loop later when diff colours available
     for (let p = 0; p < points.length-1; p++) {
-        console.log(points); // debugging
+        //console.log(points); // debugging
         if (points[p]) line(points[p][0], points[p][1], points[p+1][0], points[p+1][1]);
     }
 };
 
 var mousePressed = function() {
     if (!eraser) points.push(false, [mouseX, mouseY]);
+    console.log(`false, [${mouseX}, ${mouseY}]`);
 };
 
 var mouseDragged = function() {
@@ -44,11 +45,13 @@ var mouseDragged = function() {
         }
     } else {
         points.push([mouseX, mouseY]);
+        console.log(`[${mouseX}, ${mouseY}]`);
     }
 };
 
 var mouseReleased = function() {
     if (!eraser) points.push([mouseX, mouseY]);
+    console.log(`[${mouseX}, ${mouseY}]`);
 };
 
 var windowResized = function() {
