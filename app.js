8cc86f29e8ed192b5f424ea5f6f401f5de0156b8
Hammer-01 <98681433+Hammer-01@users.noreply.github.com>
2022-03-28 21:54:59 +1000

    Update app.js

--- app.js
+++ app.js
@@ -20,14 +20,13 @@ var setup = function() {
     
     strokeColour = color(0);
     
-    eraseFn = p => {console.log(this); return p ? p[0]+this/2 >= mouseX && p[0]-this/2 <= mouseX && p[1]+this/2 >= mouseY && p[1]-this/2 <= mouseY : false};
+    eraseFn = p => p ? p[0]+eraserSize/2 >= mouseX && p[0]-eraserSize/2 <= mouseX && p[1]+eraserSize/2 >= mouseY && p[1]-eraserSize/2 <= mouseY : false;
 };
 
 var draw = function() {
     background(255);
     stroke(strokeColour); // move inside loop later when diff colours available
     for (let p = 0; p < points.length-1; p++) {
-        //console.log(points); // debugging
         if (points[p] && points[p+1]) line(points[p][0], points[p][1], points[p+1][0], points[p+1][1]);
     }
 };
@@ -38,9 +37,9 @@ var mousePressed = function() {
 
 var mouseDragged = function() {
     if (eraser) {
-        console.log(points.findIndex(eraseFn, eraserSize));
-        while (points.findIndex(eraseFn, eraserSize) !== -1) {
-            points.splice(points.findIndex(eraseFn, eraserSize), 1);
+        console.log(points.findIndex(eraseFn));
+        while (points.findIndex(eraseFn) !== -1) {
+            points.splice(points.findIndex(eraseFn));
             console.log('in erase while loop');
         }
     } else {
